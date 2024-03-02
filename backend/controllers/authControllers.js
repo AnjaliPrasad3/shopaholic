97b95ce4ae13cda0603ea6f15330c49242e6ea7d
AnjaliPrasad3 <95201486+AnjaliPrasad3@users.noreply.github.com>
2024-03-02 20:20:11 +0530

    Add files via upload

---
+++ backend/controllers/authControllers.js
@@ -0,0 +1,53 @@
+import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
+import User from "../models/user.js";
+import ErrorHandler from "../utils/errorHandler.js";
+import sendToken from "../utils/sendToken.js";
+
+//Register user  =>  /api/v1/register
+export const registerUser = catchAsyncErrors(async (req, res, next) => {
+    const { name, email, password } = req.body;
+    const user = await User.create({
+        name,
+        email,
+        password,
+    });
+
+    sendToken(user, 201, res);
+});
+
+//Login user  =>  /api/v1/login
+export const loginUser = catchAsyncErrors(async (req, res, next) => {
+    const { email, password } = req.body;
+    
+    if(!email || !password) {
+        return next(new ErrorHandler('Please enter email & password', 400))
+    }
+
+    //Find user in the database
+    const user = await User.findOne({ email }).select("+password")
+
+    if(!user) {
+        return next(new ErrorHandler('Invalid email or password', 401))
+    }
+
+    // Check if password is correct
+    const isPasswordMatched = await user.comparePassword(password)
+
+    if(!isPasswordMatched) {
+        return next(new ErrorHandler('Invalid email or password', 401))
+    }
+
+    sendToken(user, 200, res);
+});
+
+//Logout user => /api/v1/logout
+export const logout = catchAsyncErrors( async (req, res) => {
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true,
+    });
+
+    res.status(200).json({
+        message: "Logged Out",
+    });
+});
---
+++ backend/controllers/productControllers.js
@@ -0,0 +1,81 @@
+import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
+import Product from "../models/product.js";
+import ErrorHandler from '../utils/errorHandler.js'
+import APIFilters from '../utils/apiFilters.js'
+
+//Create new Product  => /api/v1/products
+export const getProducts = catchAsyncErrors( async (req, res) => {
+    const resPerPage = 4;
+    const apiFilters = new APIFilters(Product, req.query).search().filters();
+
+    console.log("req?.user",req?.user);
+    
+    let products = await apiFilters.query;
+    let filteredProductsCount = products.length;
+
+    apiFilters.pagination(resPerPage);
+    products = await apiFilters.query.clone();
+
+    res.status(200).json({
+        resPerPage,
+        filteredProductsCount,
+        products,
+    });
+});
+
+//Create new Product  => /api/v1/admin/products
+export const newProduct = catchAsyncErrors( async (req, res) => {
+
+    req.body.user = req.user._id;
+
+   const product = await Product.create(req.body);
+
+   res.status(200).json({
+    product,
+   });
+});
+
+// Get single product details  => /api/shopaholic/products/:id
+export const getProductDetails = catchAsyncErrors( async (req, res, next) => {
+    const product = await Product.findById(req?.params?.id);
+
+    if(!product) {
+        return next(new ErrorHandler("Product not found", 404));
+    }
+
+    res.status(200).json({
+        product,
+    });
+});
+
+// Update product details  => /api/v1/products/:id
+export const updateProduct = catchAsyncErrors( async (req, res) => {
+    let product = await Product.findById(req?.params?.id);
+
+    if(!product) {
+        return next(new ErrorHandler("Product not found", 404));
+    }
+
+    product = await Product.findByIdAndUpdate(req?.params?.id, req.body, {
+        new: true,
+    });
+
+    res.status(200).json({
+        product,
+    });
+});
+
+// Delete product  => /api/v1/products/:id
+export const deleteProduct = catchAsyncErrors( async (req, res) => {
+    let product = await Product.findById(req?.params?.id);
+
+    if(!product) {
+        return next(new ErrorHandler("Product not found", 404));
+    }
+
+    await product.deleteOne();
+
+    res.status(200).json({
+        message: "Product Deleted",
+    });
+});
