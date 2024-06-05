87677019ded9036799e51fa403ad8fd11baec38c
AnjaliPrasad3 <95201486+AnjaliPrasad3@users.noreply.github.com>
2024-06-05 09:24:19 +0530

    Add files via upload

---
+++ frontend/src/redux/api/productsApi.js
@@ -0,0 +1,41 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const productApi = createApi({
+    reducerPath: "productApi",
+    baseQuery: fetchBaseQuery({ baseUrl: "/api/v1" }),
+    tagTypes: ["Product"],
+    endpoints: (builder) => ({
+        getProducts: builder.query({
+            query: (params) => ({
+                url: "/products",
+                params: {
+                    page: params?.page,
+                    keyword: params?.keyword,
+                    category: params?.category,
+                    "price[gte]": params?.min,
+                    "price[lte]": params?.max,
+                    "ratings[gte]": params?.ratings,
+                },
+            }),
+        }),
+        getProductDetails: builder.query({
+            query: (id) => ({
+                url: `/products/${id}`,
+            }),
+            providesTags: ['Product'],
+        }),
+        submitReview: builder.mutation({
+            query: (body) => ({
+                url: "/reviews",
+                method: "PUT",
+                body,
+            }),
+            invalidatesTags: ["Product"],
+        }),
+        canUserReview: builder.query({
+            query: (productId) => `/can_review/?productId=${productId}`,
+        }),
+    }),
+});
+
+export const { useGetProductsQuery, useGetProductDetailsQuery, useSubmitReviewMutation, useCanUserReviewQuery } = productApi;
