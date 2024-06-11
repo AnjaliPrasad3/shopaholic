d1b009d05eaa5c4bbafea65fdcb315c09dac24c0
AnjaliPrasad3 <95201486+AnjaliPrasad3@users.noreply.github.com>
2024-06-11 12:39:49 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+
+import { Toaster } from 'react-hot-toast';
+import useUserRoutes from "./components/routes/userRoutes";
+import useAdminRoutes from "./components/routes/adminRoutes";
+
+
+
+function App() {
+  const userRoutes = useUserRoutes();
+  const adminRoutes = useAdminRoutes();
+
+  return (
+    <Router>
+      <div className="App">
+        <Toaster position="top-center" />
+        <Header />
+        <div className="container">
+          <Routes>
+          {userRoutes}
+          {adminRoutes}
+          </Routes>
+        </div>
+
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/redux/api/orderApi.js
@@ -0,0 +1,60 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const orderApi = createApi({
+    reducerPath: "orderApi",
+    baseQuery: fetchBaseQuery({ baseUrl: "/api/v1" }),
+    tagTypes: ['Order'],
+    endpoints: (builder) => ({
+        
+        createNewOrder: builder.mutation({
+            query(body) {
+                return {
+                    url: "/orders/new",
+                    method: "POST",
+                    body,
+                };
+            },
+        }),
+
+        myOrders: builder.query({
+            query: () => `/me/orders`,
+        }),
+
+        orderDetails: builder.query({
+            query: (id) => `/orders/${id}`,
+            providesTags: ['Order']
+        }),
+
+        stripeCheckoutSession: builder.mutation({
+            query(body) {
+                return {
+                    url: "/payment/checkout_session",
+                    method: "POST",
+                    body,
+                };
+            },
+        }),
+
+        getDashboardSales: builder.query({
+            query: ({startDate, endDate}) => 
+                `/admin/get_sales?startDate=${startDate}&endDate=${endDate}`,
+        }),
+
+        getAdminOrders: builder.query({
+            query: () => `/admin/orders`,
+        }),
+
+        updateOrder: builder.mutation({
+            query({id, body}) {
+                return {
+                    url: "/admin/orders/${id}",
+                    method: "PUT",
+                    body,
+                };
+            },
+            invalidatesTags: ['Order']
+        }),
+    }),
+});
+
+export const { useCreateNewOrderMutation, useStripeCheckoutSessionMutation, useMyOrdersQuery, useOrderDetailsQuery, useLazyGetDashboardSalesQuery, useGetAdminOrdersQuery, useUpdateOrderMutation } = orderApi;
